45b44c550c26c6ebb41a17dcfcc6c99c0b1b91b0
m4teo <m4teoarg@gmail.com>
2024-04-15 21:21:03 -0300

    colorizer update

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,3 +1,17 @@
+local function colorizer_config(_, opts)
+    local colorizer = require("colorizer")
+    colorizer.setup(opts)
+
+    vim.defer_fn(function()
+        colorizer.attach_to_buffer(0)
+    end, 0)
+end
+
 return {
-    { 'NvChad/nvim-colorizer.lua' }
+    {
+        "NvChad/nvim-colorizer.lua",
+        lazy = true,
+        config = colorizer_config,
+        ft = "css"
+    }
 }
